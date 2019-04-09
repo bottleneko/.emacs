fa4a697ce0c6b4d5cbf4e0cbeb608416b2a9e54b
Boris Murashov <bottleneko@gmail.com>
2019-04-09 17:50:14 +0300

    ElDoc background

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(lsp-ui-doc-background ((t (:background "#000000")))))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
