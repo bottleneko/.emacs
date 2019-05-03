6d93e2b0f27fd4c9c4eb732c16349ac041d8bd43
Boris Murashov <bottleneko@gmail.com>
2019-05-03 23:59:49 +0300

    Colorize compilation buffer

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (gitlab-ci-mode racer flycheck-rust lsp-scala lsp-ui cargo auto-org-md ensime json-mode php-mode evil-lion code-stats counsel ivy ivy-erlang-complete magit dashboard flyspell-correct-ivy all-the-icons-ivy counsel-projectile ivy-gitlab ivy-xref ivy-yasnippet company company-web use-package xclip latex-pretty-symbols latex-preview-pane dockerfile-mode haskell-mode rust-mode evil bash-completion markdown-mode markdown-preview-mode ansible ansible-doc bind-key iedit switch-buffer-functions neotree flycheck-tip eclim flycheck doom-themes ample-theme projectile exec-path-from-shell ssh-config-mode rainbow-delimiters k8s-mode erlang))))
+    (elnode docker-tramp gitlab-ci-mode racer flycheck-rust lsp-scala lsp-ui cargo auto-org-md ensime json-mode php-mode evil-lion code-stats counsel ivy ivy-erlang-complete magit dashboard flyspell-correct-ivy all-the-icons-ivy counsel-projectile ivy-gitlab ivy-xref ivy-yasnippet company company-web use-package xclip latex-pretty-symbols latex-preview-pane dockerfile-mode haskell-mode rust-mode evil bash-completion markdown-mode markdown-preview-mode ansible ansible-doc bind-key iedit switch-buffer-functions neotree flycheck-tip eclim flycheck doom-themes ample-theme projectile exec-path-from-shell ssh-config-mode rainbow-delimiters k8s-mode erlang))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -271,3 +271,12 @@ White space here is any of: space, tab, Emacs newline (line feed, ASCII 10)."
           ((eq key ?f) (insert "φ")))))
 
 (define-key global-map (kbd "M-g s") 'greek-transliteration)
+
+(use-package ansi-color
+  :hook ((compilation-filter . colorize-compilation))
+  :init
+  (defun colorize-compilation ()
+    "Colorize from `compilation-filter-start' to `point'."
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region
+       compilation-filter-start (point)))))
